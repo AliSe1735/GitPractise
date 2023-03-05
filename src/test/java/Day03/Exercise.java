806754db4d8be078104b1a963afcf6059e08b1d8
AliSe1735 <ali.serdan89@gmail.com>
2023-03-05 14:39:05 +0300

    changes added

---
+++ src/test/java/Day03/Exercise.java
@@ -0,0 +1,11 @@
+package Day03;
+
+public class Exercise {
+    public static void main(String[] args) {
+        int a = 20;
+        String bA = Integer.toBinaryString(a);
+
+        System.out.println(a);
+        System.out.println(bA);
+    }
+}
