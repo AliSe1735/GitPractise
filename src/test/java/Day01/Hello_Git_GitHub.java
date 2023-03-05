cd73f217891c3e4b919654a6fd3001bf753c4d87
Ali_SE <serdan@alis-MacBook-Pro.local>
2023-03-05 14:21:51 +0300

    first class created

---
+++ src/test/java/Day01/Hello_Git_GitHub.java
@@ -0,0 +1,8 @@
+package Day01;
+
+public class Hello_Git_GitHub {
+    public static void main(String[] args) {
+        System.out.println("We started Git");
+        System.out.println("We started GitHub");
+    }
+}
