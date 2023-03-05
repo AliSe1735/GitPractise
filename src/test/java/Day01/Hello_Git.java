dc56abb81d574e3ca1ba6098dc847d8ff0070861
AliSE1735 <ali.serdan89@gmail.com>
2023-03-05 14:23:39 +0300

    class name changed

--- src/test/java/Day01/Hello_Git.java
+++ src/test/java/Day01/Hello_Git.java
@@ -1,6 +1,6 @@
 package Day01;
 
-public class Hello_Git_GitHub {
+public class Hello_Git {
     public static void main(String[] args) {
         System.out.println("We started Git");
         System.out.println("We started GitHub");
